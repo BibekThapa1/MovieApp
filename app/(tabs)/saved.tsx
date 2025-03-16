e8927c32db6998d82c6ba672cef83de3dcbc6b95
Bibek Thapa <bibekthapa00f@gmail.com>
2025-03-16 11:04:12 +0545

    Initial commit

---
+++ app/(tabs)/saved.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const saved = () => {
+  return (
+    <View>
+      <Text className='flex items-center justify-center text-5xl'>saved</Text>
+    </View>
+  )
+}
+
+export default saved
