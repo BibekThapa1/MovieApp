dfd1c5c1605fdfae335376a341259b973a106a6f
Bibek Thapa <bibekthapa00f@gmail.com>
2025-03-20 00:31:13 +0545

    completed something

--- app/movies/[id].tsx
+++ app/movies/[id].tsx
@@ -1,12 +1,51 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, ScrollView, Image } from "react-native";
+import React from "react";
+import { useLocalSearchParams } from "expo-router";
+import useFetch from "@/services/useFetch";
+import { fetchMovieDetails } from "@/services/api";
+import { icons } from "@/constants/icons";
 
 const MovieDetails = () => {
-  return (
-    <View>
-      <Text>MovieDetails</Text>
-    </View>
-  )
-}
+  const { id } = useLocalSearchParams();
+  console.log("Movie details id:", id);
 
-export default MovieDetails
+  const { data: movie, loading } = useFetch(() =>
+    fetchMovieDetails(id as string)
+  );
+
+  return (
+    <View className="bg-primary flex-1">
+      <ScrollView contentContainerStyle={{ paddingBottom: 80 }}>
+        <View>
+          <Image
+            source={{
+              uri: `https://image.tmdb.org/t/p/w500${movie?.poster_path}`,
+            }}
+            className="w-full h-[550px]"
+            resizeMode="stretch"
+          />
+        </View>
+        <View className="flex-col items-start justify-center mt-5 px-5">
+          <Text className="text-white font-bold text-xl">{movie?.title}</Text>
+        </View>
+        <View className="flex-row items-center gap-x-1 mt-2">
+          <Text className="text-light-200 text-sm">
+            {movie?.release_date?.split("-")[0]}
+            <Text className="text-light-200 text-sm pl-4">
+              {movie?.runtime}m
+            </Text>
+          </Text>
+        </View>
+        <View className="flex-row items-center bg-dark-100 px-2 py-1 rounded-md gap-x-1 mt-2">
+          <Image source={icons.star} className="size-4"/>
+          <Text className="text-white font-bold text-sm">
+            {Math.round(movie?.vote_average ?? 0)} /10
+          </Text>
+          <Text className="text-light-200 text-sm">({movie?.vote_count} votes)</Text>
+        </View>
+      </ScrollView>
+    </View>
+  );
+};
+
+export default MovieDetails;
--- services/api.ts
+++ services/api.ts
@@ -7,7 +7,6 @@ export const TMDB_CONFIG = {
   },
 };
 
-
 export const fetchMovies = async ({ query }: { query: string }) => {
   const endpoint = query
     ? `${TMDB_CONFIG.BASE_URL}/search/movie?query=${encodeURIComponent(query)}`
@@ -25,4 +24,32 @@ export const fetchMovies = async ({ query }: { query: string }) => {
   const data = await response.json();
 
   return data.results;
+};
+
+export const fetchMovieDetails = async (
+  movieId: string
+): Promise<MovieDetails> => {
+  try {
+    console.log("Entered4 here");
+    
+    const response = await fetch(
+      `${TMDB_CONFIG.BASE_URL}/movie/${movieId}?api_key=${TMDB_CONFIG.API_KEY}`,
+      {
+        method: "GET",
+        headers: TMDB_CONFIG.headers,
+      }
+    );
+
+    if (!response.ok) {
+      throw new Error("Failed to fetch movies or movie details");
+    }
+
+    const data = await response.json();
+    console.log(data);
+    
+    return data;
+  } catch (error) {
+    console.log("Movie details fetching error",error);
+    throw error;
+  }
 };
